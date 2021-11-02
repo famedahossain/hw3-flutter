4928370125b6e228cf916f459ff055e6dfc69dc4
fameda <famedahossain@gmail.com>
2021-11-01 21:42:13 -0400

    Image Change

--- lib/chat.dart
+++ lib/chat.dart
@@ -14,14 +14,14 @@ CollectionReference users = FirebaseFirestore.instance.collection('users');
 
 class _ChatPageState extends State<Chat> {
   List<Users> chatUsers = [
-    Users(name: "Jeon Jungkook", messageText: "I know name, but not movie", imageURL: "assets/user_jungkook.jpg", time: "Now"),
-    Users(name: "Kim Seokjin", messageText: "Hilarious!", imageURL: "assets/user_jin.jpg", time: "Yesterday"),
-    Users(name: "Min Yoongi", messageText: "What is a lil meow meow?", imageURL: "assets/user_yoongi.jpg", time: "31 Oct"),
-    Users(name: "Kim Taehyung", messageText: "Do you know purple?", imageURL: "assets/user_v.jpg", time: "23 Oct"),
-    Users(name: "Park Jimin", messageText: "You nice, keep going.", imageURL: "assets/user_jimin.jpg", time: "12 Oct"),
-    Users(name: "Jung Hoseok", messageText: "But can you even dance though?", imageURL: "assets/user_jhope.jpg", time: "22 Sept"),
-    Users(name: "Kim Namjoon", messageText: "Have you seen my bonsai tree recently?", imageURL: "assets/user_rm.jpg", time: "1 Sept"),
-    Users(name: "Bang Sihyuk", messageText: "What are the newest money making schemes?",imageURL: "assets/user_bangpd.jpg", time: "16 Aug"),
+    Users(name: "Jeon Jungkook", messageText: "I know name, but not movie", imageURL: "https://kpopping.com/documents/94/3/2000/batch_LIV_4178-copy.jpeg", time: "Now"),
+    Users(name: "Kim Seokjin", messageText: "Hilarious!", imageURL: "https://i.pinimg.com/474x/3d/cc/81/3dcc81ff5f9612640c6da87c80b4b791.jpg", time: "Yesterday"),
+    Users(name: "Min Yoongi", messageText: "What is a lil meow meow?", imageURL: "https://filmdaily.co/wp-content/uploads/2020/09/HotSuga-7-A-pretty-boy-if-there-ever-was-one72.jpg", time: "31 Oct"),
+    Users(name: "Kim Taehyung", messageText: "Do you know purple?", imageURL: "https://pbs.twimg.com/media/EgE6DoLXsAEowLk.jpg", time: "23 Oct"),
+    Users(name: "Park Jimin", messageText: "You nice, keep going.", imageURL: "http://images6.fanpop.com/image/photos/41400000/X-DISPATCH-FOR-Jimin-5TH-ANNIVERSARY-bts-41419921-422-588.png", time: "12 Oct"),
+    Users(name: "Jung Hoseok", messageText: "But can you even dance though?", imageURL: "https://cdn-contents.magazine.weverse.io/upload/img/202011/20201124i8Jw6N7LT9o0.jpg", time: "22 Sept"),
+    Users(name: "Kim Namjoon", messageText: "Have you seen my bonsai tree recently?", imageURL: "https://pbs.twimg.com/media/EA92mW2UEAcvhXX.jpg", time: "1 Sept"),
+    Users(name: "Bang Sihyuk", messageText: "What are the newest money making schemes?",imageURL: "https://pbs.twimg.com/profile_images/671753737065652225/y8-36RNE_400x400.jpg", time: "16 Aug"),
   ];
   @override
   Widget build(BuildContext context) {
